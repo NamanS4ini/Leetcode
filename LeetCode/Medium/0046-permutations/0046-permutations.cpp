90e7a2e6ddc6158f69ff9968bc61d45fd1efd38e
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-07-09 09:47:39 +0530

    0046-permutations 07-09-2025

--- LeetCode/Medium/0046-permutations/0046-permutations.cpp
+++ LeetCode/Medium/0046-permutations/0046-permutations.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void solve(vector<int> nums, vector<vector<int>>& ans, int index){
         if(index >= nums.size()){
-                ans.push_back(nums);
+            ans.push_back(nums);
             return;
         }
         for(int i = index; i < nums.size(); i++){
