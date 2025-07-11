4c7798ee69ff7bb732ce55f554f3995151e134a3
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-07-11 11:17:11 +0530

    0015-3sum 07-11-2025

---
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int i = 0; i < nums.size(); i++){
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            int left = i+1;
+            int right = nums.size() - 1;
+            while(left < right){
+                if(nums[i] + nums[left] + nums[right] == 0){
+                    ans.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    while(nums[left] == nums[left - 1] && left < right)
+                        left++;
+                    right--;
+                }
+                else if(nums[i] + nums[left] + nums[right] > 0){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
+        }
+        return ans;
+    }
+};
