2eaae3350d2bc299b820df1f84ab9267bb68db78
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-07-14 13:37:11 +0530

    0560-subarray-sum-equals-k 07-14-2025

--- LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ LeetCode/Medium/0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -3,11 +3,8 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int count = 0;
         for (int i = 0; i < nums.size(); i++) {
-            int sum = nums[i];
-                if (sum == k) {
-                    count++;
-                }
-            for (int j = i + 1; j < nums.size(); j++) {
+            int sum = 0;
+            for (int j = i; j < nums.size(); j++) {
                 sum += nums[j];
                 if (sum == k) {
                     count++;
