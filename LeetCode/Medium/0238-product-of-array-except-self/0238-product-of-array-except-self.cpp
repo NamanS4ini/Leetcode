f144571c2a0e2f30c1a808d2538ee6883906ec20
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-07-22 17:12:24 +0530

    0238-product-of-array-except-self 07-22-2025

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> prefix(nums.size(), 1);
         prefix[0] = nums[0];
-        for(int i = 1; i < nums.size(); i++){
-            prefix[i] = prefix[i-1] * nums[i];
-        }
 
         vector<int> suffix(nums.size(), 1);
         suffix[nums.size()-1] = nums[nums.size()-1];
-        for(int i = nums.size() - 2; i >= 0; i--){
-            suffix[i] = suffix[i + 1] * nums[i];
+
+        for(int i = 1; i < nums.size(); i++){
+            prefix[i] = prefix[i-1] * nums[i];
+            suffix[nums.size() - i - 1] = suffix[nums.size() - i] * nums[nums.size() - i - 1];
         }
+
         vector<int> ans(nums.size(), 0);
         for(int i = 0; i < nums.size(); i++){
             int prefVal = i - 1 >= 0 ? prefix[i-1] : 1;
