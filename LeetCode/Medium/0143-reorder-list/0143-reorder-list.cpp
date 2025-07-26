30e6112105fd45374546cb4d5ee669f9ce45b571
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-07-26 15:23:19 +0530

    0143-reorder-list 07-26-2025

--- LeetCode/Medium/0143-reorder-list/0143-reorder-list.cpp
+++ LeetCode/Medium/0143-reorder-list/0143-reorder-list.cpp
@@ -11,25 +11,34 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        if(head -> next == nullptr){
-            return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast -> next && fast -> next -> next){
+            slow = slow -> next;
+            fast = fast -> next -> next;
         }
-        vector<ListNode*> nodes;
-        ListNode* temp = head;
-        while (temp != nullptr) {
-            nodes.push_back(temp);
-            temp = temp->next;
+        ListNode* l1 = head;
+        ListNode* l2 = slow -> next;
+        slow -> next = nullptr;
+
+        ListNode* prev = nullptr;
+        ListNode* curr = l2;
+        while(curr){
+            ListNode* next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
         }
-        int left = 0;
-        int right = nodes.size() - 1;
-        while (left < right) {
-            nodes[left]->next = nodes[right];
-            left++;
-            if (left < right) {
-                nodes[right]->next = nodes[left];
-                right--;
-            }
+        l2 = prev;
+
+        while(l1 && l2){
+            ListNode* l1Next = l1->next;
+            ListNode* l2Next = l2->next;
+            l1 -> next = l2;
+            l2->next = l1Next;
+            l1 = l1Next;
+            l2 = l2Next;
         }
-        nodes[left]->next = nullptr;
+
     }
 };
