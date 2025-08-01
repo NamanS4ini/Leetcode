eb006440ee4c1d25d5c79ec505082bb67d989f53
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-01 13:44:30 +0530

    0739-daily-temperatures 08-01-2025

--- LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
+++ LeetCode/Medium/0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        vector<int> ans(temperatures.size(),0);
+        vector<int> ans(temperatures.size(), 0);
         stack<vector<int>> s;
-        for(int i = temperatures.size() - 1; i >= 0; i--){
-            if(s.empty()){
-                s.push({i,temperatures[i]});
+        for (int i = temperatures.size() - 1; i >= 0; i--) {
+            while (!s.empty() && s.top()[1] <= temperatures[i]) {
+                s.pop();
             }
-            else{
-                while(!s.empty() && s.top()[1] <= temperatures[i]){
-                    s.pop();
-                }
-                if(!s.empty())
-                    ans[i] = s.top()[0] - i;
-                s.push({i,temperatures[i]});
-            }
+            if (!s.empty())
+                ans[i] = s.top()[0] - i;
+            s.push({i, temperatures[i]});
         }
         return ans;
     }
