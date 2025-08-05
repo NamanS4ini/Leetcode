105c06cb3efdd47decf9329db22bbcfa399df626
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-05 19:26:17 +0530

    3477-fruits-into-baskets-ii 08-05-2025

---
+++ LeetCode/Easy/3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        for(int i = 0; i < fruits.size(); i++){
+            for(int j = 0; j < baskets.size(); j++){
+                if(baskets[j] >= fruits[i]){
+                    baskets.erase(baskets.begin() + j);
+                    break;
+                }
+            }
+        }
+        return baskets.size();
+    }
+};
