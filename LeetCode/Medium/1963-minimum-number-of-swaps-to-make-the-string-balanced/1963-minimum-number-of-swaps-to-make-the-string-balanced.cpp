e9aeff6c5ffd9a02bfc0815ba0b94a7cd76b7b54
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-07 21:59:01 +0530

    1963-minimum-number-of-swaps-to-make-the-string-balanced 08-07-2025

--- LeetCode/Medium/1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ LeetCode/Medium/1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int close = 0;
-        int open = 0;
-        int swaps = 0;
+        stack<char> st;
+        int left = 0;
         int right = s.length() - 1;
-        for(int i = 0; i < right; i++){
-            char c = s[i];
-            if(c == '['){
-                open++;
+        int swaps = 0;
+        while(left < right){
+            if(s[left] == '['){
+                st.push('[');
             }
             else{
-                if(close + 1 <= open){
-                    close++;
-                }
+                if(!st.empty())
+                    st.pop();
                 else{
                     while(s[right] != '['){
                         right--;
                     }
                     right--;
                     swaps++;
+                    st.push('[');
                 }
             }
+            left++;
         }
-        return ceil(swaps/2.0);
+        return swaps;
     }
 };
