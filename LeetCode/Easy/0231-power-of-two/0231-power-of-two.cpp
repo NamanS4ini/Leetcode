6b877a9209fcf471534ff23ca1a4dedfb717b267
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-09 06:54:32 +0530

    0231-power-of-two 08-09-2025

---
+++ LeetCode/Easy/0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n < 1)
+            return false;
+        for(int i = 0; i < 32; i++){
+            if(pow(2,i) == n){
+                return true;
+            }
+        }
+        return false;
+    }
+};
