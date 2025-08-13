1d17f8956e490221809906cbcf64ffaf0d26bc92
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-13 11:20:28 +0530

    0042-trapping-rain-water 08-13-2025

--- LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        int maxLeft = height[0]; 
+        int maxRight = height[height.size() - 1];
         int left = 0;
         int right = height.size() - 1;
-        int maxLeft = 0;
-        int maxRight = 0;
         int water = 0;
         while(left < right){
             if(height[left] < height[right]){
-                maxLeft = max(maxLeft, height[left]);
-                water += maxLeft - height[left];
                 left++;
+                maxLeft = max(height[left], maxLeft);
+                water += maxLeft - height[left];
             }
             else{
-                maxRight = max(maxRight, height[right]);
-                water += maxRight - height[right];
                 right--;
+                maxRight = max(height[right], maxRight);
+                water += maxRight - height[right];
             }
         }
         return water;
