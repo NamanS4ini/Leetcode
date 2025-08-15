e3fbfa4be808f87c484075f1c257a99971834389
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-15 06:59:31 +0530

    0342-power-of-four 08-15-2025

---
+++ LeetCode/Easy/0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        long long num = 1;
+        while(num <= n){
+            if(num == n)
+                return true;
+            num *= 4;
+        }
+        return false;
+    }
+};
