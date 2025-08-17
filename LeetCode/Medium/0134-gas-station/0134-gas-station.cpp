9d29004b671052c4deb1fc6b09d3014d94b32d10
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-17 17:53:32 +0530

    0134-gas-station 08-17-2025

---
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalGas = 0;
+        int totalCost = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            totalGas += gas[i];
+            totalCost += cost[i];
+        }
+        if (totalGas < totalCost) {
+            return -1;
+        }
+        int fuel = 0;
+        int start = 0;
+        for (int i = 0; i < gas.size(); i++) {
+            fuel += gas[i] - cost[i];
+            if(fuel < 0){
+                fuel = 0;
+                start = i + 1;
+            }
+        }
+        return start;
+    }
+};
