891856e7f1b950ffca1dc93331978241d58e1ebb
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-19 20:31:49 +0530

    0226-invert-binary-tree 08-19-2025

--- LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ LeetCode/Easy/0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -12,13 +12,14 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(root == NULL)
-            return NULL;
-        invertTree(root -> left);
-        invertTree(root -> right);
-        TreeNode* temp = root-> left;
-        root-> left = root -> right;
-        root ->right = temp;
+        if(!root)
+            return nullptr;
+        invertTree(root->right);
+        invertTree(root->left);
+
+        TreeNode* temp = root -> left;
+        root->left = root -> right;
+        root-> right = temp;
         return root;
     }
 };
