95c6a4a919ed91eae410640c2700585a83381d85
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-23 14:08:17 +0530

    2461-maximum-sum-of-distinct-subarrays-with-length-k 08-23-2025

--- LeetCode/Medium/2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ LeetCode/Medium/2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
+        int maxSum = 0;
+        unordered_set<int> seen;
         int left = 0;
         int right = 0;
-        long long sum = 0;
-        unordered_set<long long> seen;
-        long long maxSum = 0;
+        int currSum = 0;
         while (right < nums.size()) {
-            if(seen.find(nums[right]) == seen.end()){
-                seen.insert(nums[right]);
-                sum += nums[right];
-                if(right - left + 1 == k){
-                    maxSum = max(maxSum, sum);
-                    sum -= nums[left];
-                    seen.erase(nums[left++]);
+            if (right - left + 1 < k) {
+                if (seen.find(nums[right]) == seen.end()) {
+                    seen.insert(nums[right]);
+                    currSum += nums[right];
+                    right++;
+                } else {
+                    seen.erase(nums[left]);
+                    currSum -= nums[left];
+                    left++;
                 }
-                right++;
-            }
-            else{
-                sum -= nums[left];
-                seen.erase(nums[left++]);
+            } else {
+                if (seen.find(nums[right]) != seen.end()) {
+                    while (seen.find(nums[right]) != seen.end()) {
+                        seen.erase(nums[left]);
+                        currSum -= nums[left];
+                        left++;
+                    }
+                } else {
+                    seen.insert(nums[right]);
+                    currSum += nums[right];
+                    maxSum = max(maxSum, currSum);
+                    seen.erase(nums[left]);
+                    currSum -= nums[left];
+                    left++;
+                    right++;
+                }
             }
         }
         return maxSum;
