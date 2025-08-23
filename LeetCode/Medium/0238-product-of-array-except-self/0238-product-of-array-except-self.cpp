49f3e17944c821cbfcb4a210e30e960107b0d19f
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-23 14:32:29 +0530

    0238-product-of-array-except-self 08-23-2025

--- LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ LeetCode/Medium/0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,19 +3,19 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> prefix(nums.size());
         prefix[0] = nums[0];
-        for(int i = 1; i < nums.size(); i++)
-            prefix[i] = nums[i] * prefix[i - 1];
+        for(int i = 1; i < nums.size(); i++){
+            prefix[i] = prefix[i - 1] * nums[i];
+        }
         vector<int> suffix(nums.size());
         suffix[nums.size() - 1] = nums[nums.size() - 1];
         for(int i = nums.size() - 2; i >= 0; i--){
-            suffix[i] = nums[i] * suffix[i+1];
+            suffix[i] = suffix[i + 1] * nums[i];
         }
-
         vector<int> ans(nums.size());
-        for(int i = 0; i < nums.size(); i++){
-            int pre = i > 0 ? prefix[i-1] : 1;
-            int suff = i < nums.size() - 1 ? suffix[i+1] : 1;
-            ans[i] = pre*suff;
+        ans[0] = suffix[1];
+        ans[nums.size() - 1] = prefix[nums.size() - 2];
+        for(int i = 1; i < nums.size() - 1; i++){
+            ans[i] = prefix[i - 1] * suffix[i + 1];
         }
         return ans;
     }
