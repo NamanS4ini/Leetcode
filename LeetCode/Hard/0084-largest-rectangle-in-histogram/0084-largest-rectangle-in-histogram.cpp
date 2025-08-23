d4ad185389495d332d3fe594bb7f080426e2ee22
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-23 20:53:51 +0530

    0084-largest-rectangle-in-histogram 08-23-2025

--- LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ LeetCode/Hard/0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int maxArea = 0;
-        stack <pair<int, int>> s;
-        for (int i = 0; i < heights.size(); i++) {
-            int element = heights[i];
-            int startIndex = i;
-            while (!s.empty() && s.top().second > element) {
-                startIndex = s.top().first;
-                maxArea = max(maxArea, (i - s.top().first) * s.top().second);
-                s.pop();
+        stack<pair<int, int>> s;
+        for(int i = 0; i < heights.size(); i++){
+            if(s.empty() || heights[i] > s.top().second){
+                s.push({i, heights[i]});
             }
-            s.push({startIndex, element});
+            else{
+                int start = i;
+                while(!s.empty() && s.top().second > heights[i]){
+                    start = s.top().first;
+                    int area = (i - start) * s.top().second;
+                    maxArea = max(maxArea, area);
+                    s.pop();
+                }
+                s.push({start, heights[i]});
+            }
         }
-        while (!s.empty()) {
-            int currArea = (heights.size() - s.top().first) * s.top().second;
-            maxArea = max(maxArea, currArea);
+        while(!s.empty()){
+            int area = (heights.size() - s.top().first) * s.top().second;
+            maxArea = max(maxArea, area);
             s.pop();
         }
         return maxArea;
