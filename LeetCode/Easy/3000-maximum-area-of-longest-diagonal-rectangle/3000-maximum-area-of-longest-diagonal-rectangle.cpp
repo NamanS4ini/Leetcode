5b84f7a857fca3db2cb025e9ae4313eaca9fc2b7
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-26 06:24:17 +0530

    3000-maximum-area-of-longest-diagonal-rectangle 08-26-2025

--- LeetCode/Easy/3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.cpp
+++ LeetCode/Easy/3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -4,8 +4,7 @@ public:
         float maxDiagonal = 0;
         int maxArea = 0;
         for(vector<int> rect: dimensions){
-            float diagonal = sqrt(rect[0] * rect[0] + rect[1] * rect[1]);
-            cout << diagonal << " ";
+            float diagonal = rect[0] * rect[0] + rect[1] * rect[1];
             if(maxDiagonal < diagonal){
                 maxDiagonal = diagonal;
                 maxArea = rect[0] * rect[1];
