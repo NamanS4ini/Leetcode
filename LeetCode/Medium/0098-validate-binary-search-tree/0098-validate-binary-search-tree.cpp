a404e27163cb037b361572541d01770c8d6dcdd4
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-28 19:08:17 +0530

    0098-validate-binary-search-tree 08-28-2025

---
+++ LeetCode/Medium/0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void inOrder(TreeNode* root, vector<int>& ans){
+        if(!root)
+            return;
+        inOrder(root->left, ans);
+        ans.push_back(root -> val);
+        inOrder(root->right, ans);
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        vector<int> ans;
+        inOrder(root, ans);
+        for(int i = 1; i < ans.size(); i++){
+            if(ans[i] <= ans[i-1])
+                return false;
+        }
+        return true;
+    }
+};
