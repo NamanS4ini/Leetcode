8591aef5ae6bbfe4b197570a4b20a472f37ead3b
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-30 08:52:08 +0530

    0036-valid-sudoku 08-30-2025

--- LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.cpp
+++ LeetCode/Medium/0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -3,24 +3,25 @@ public:
     bool isValidSudoku(vector<vector<char>>& board) {
         vector<unordered_set<char>> rows(9);
         vector<unordered_set<char>> cols(9);
-        vector<unordered_set<char>> boxes(9);
+        vector<unordered_set<char>> subBox(9);
 
-        for (int i = 0; i < board[0].size(); i++) {
-            for (int j = 0; j < board.size(); j++) {
-                char curr = board[i][j];
-                if (curr == '.') {
-                    continue;
-                }
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                char val = board[i][j];
                 int boxIndex = (i/3) * 3 + (j/3);
-                if (!rows[i].count(curr) && !cols[j].count(curr) && !boxes[boxIndex].count(curr)) {
-                    rows[i].insert(curr);
-                    cols[j].insert(curr);
-                    boxes[boxIndex].insert(curr);
-                } else {
-                    return false;
+                if (val != '.') {
+                    if (rows[i].find(val) == rows[i].end() &&
+                        cols[j].find(val) == cols[j].end() &&
+                        subBox[boxIndex].find(val) == subBox[boxIndex].end()) {
+                        rows[i].insert(val);
+                        cols[j].insert(val);
+                        subBox[boxIndex].insert(val);
+                    } else {
+                        return false;
+                    }
                 }
             }
         }
         return true;
-    } 
+    }
 };
