b7def61743192fdb63efd4243f4af05f3777a976
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-08-30 11:19:27 +0530

    0236-lowest-common-ancestor-of-a-binary-tree 08-30-2025

--- LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -11,11 +11,11 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root)
-            return NULL;
+            return nullptr;
 
         if(root == p || root == q)
             return root;
-        
+
         TreeNode* left = lowestCommonAncestor(root -> left, p, q);
         TreeNode* right = lowestCommonAncestor(root -> right, p, q);
 
@@ -23,7 +23,7 @@ public:
             return right;
         if(!right)
             return left;
-
+        
         return root;
     }
 };
