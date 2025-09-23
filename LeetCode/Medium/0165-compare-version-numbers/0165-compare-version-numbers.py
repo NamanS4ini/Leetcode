c0dfbcd4b121edd467bc3bb537461de4305ddcb6
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-09-23 07:06:42 +0530

    0165-compare-version-numbers 09-23-2025

--- LeetCode/Medium/0165-compare-version-numbers/0165-compare-version-numbers.py
+++ LeetCode/Medium/0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -2,15 +2,16 @@ class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         l1 = version1.split(".")
         l2 = version2.split(".")
+
         length = len(l1) if len(l1) > len(l2) else len(l2)
+
         for i in range(length):
             v1 = int(l1[i]) if i < len(l1) else 0
             v2 = int(l2[i]) if i < len(l2) else 0
+
             if(v1 > v2):
                 return 1
             elif(v2 > v1):
                 return -1
     
         return 0
-
-        
