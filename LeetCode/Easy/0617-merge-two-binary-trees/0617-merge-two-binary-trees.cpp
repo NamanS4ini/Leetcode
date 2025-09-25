6a19ca126babf462937c90a1ffa0a2c7a6bc6e6c
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-09-25 21:37:57 +0530

    0617-merge-two-binary-trees 09-25-2025

--- LeetCode/Easy/0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ LeetCode/Easy/0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -16,18 +16,22 @@ public:
             return NULL;
         int v1 = root1 ? root1->val : 0;
         int v2 = root2 ? root2->val : 0;
-        TreeNode* root = new TreeNode(v1 + v2);
+        if(root1)
+            root1-> val = v1 + v2;
+        else{
+            root1 = new TreeNode(v2);
+        }
         if(!root1){
-        root -> left = mergeTrees(root1, root2 -> left);
-        root -> right = mergeTrees(root1, root2 -> right);
+        root1 -> left = mergeTrees(root1, root2 -> left);
+        root1 -> right = mergeTrees(root1, root2 -> right);
         }
         else if(!root2){
-        root -> left = mergeTrees(root1 -> left, root2);
-        root -> right = mergeTrees(root1 -> right, root2);
+        root1 -> left = mergeTrees(root1 -> left, root2);
+        root1 -> right = mergeTrees(root1 -> right, root2);
         }else{
-        root -> left = mergeTrees(root1 -> left, root2 -> left);
-        root -> right = mergeTrees(root1 -> right, root2->right);
+        root1 -> left = mergeTrees(root1 -> left, root2 -> left);
+        root1 -> right = mergeTrees(root1 -> right, root2->right);
         }
-        return root;
+        return root1;
     }
 };
