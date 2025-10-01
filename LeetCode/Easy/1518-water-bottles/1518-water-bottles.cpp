47b8d97b1f44ed3db7d5d25b90aa0b4c962f66b7
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-10-01 07:36:29 +0530

    1518-water-bottles 10-01-2025

--- LeetCode/Easy/1518-water-bottles/1518-water-bottles.cpp
+++ LeetCode/Easy/1518-water-bottles/1518-water-bottles.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
-    int numWaterBottles(int numBottles, int numExchange) {
-        int total = numBottles;
-        while(numBottles >= numExchange){
-            total += numBottles/numExchange;
-            numBottles = (numBottles/numExchange) + (numBottles%numExchange);
-        }
-        return total;
+    int numWaterBottles(int n, int a) {
+        return n + ((n-1)/(a-1));
     }
 };
