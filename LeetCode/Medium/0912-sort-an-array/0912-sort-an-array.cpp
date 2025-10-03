61ef6e4e914f62d214306edeb4e1aa7570ede84a
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-10-03 14:22:39 +0530

    0912-sort-an-array 10-03-2025

---
+++ LeetCode/Medium/0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    void heapify(vector<int>& nums, int size, int index){
+        int largest = index;
+        int left = 2 * index + 1;
+        int right = (2 * index) + 2;
+        if(left <= size && nums[largest] < nums[left]){
+            largest = left;
+        }
+        if(right <= size && nums[largest] < nums[right]){
+            largest = right;
+        }
+        if(largest != index){
+            swap(nums[largest], nums[index]);
+            heapify(nums, size, largest);
+        }
+    }
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        int size = nums.size() - 1;
+        for(int i = size/2; i >= 0; i--){
+            heapify(nums, size, i);
+        }
+        for(int num: nums)
+            cout << num;
+        for(int i = nums.size() - 1; i > 0; i--){
+            swap(nums[0], nums[i]);
+            heapify(nums, --size, 0);
+        }
+        return nums;
+    }
+};
