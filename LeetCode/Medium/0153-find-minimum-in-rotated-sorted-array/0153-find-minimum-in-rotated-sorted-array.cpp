6e068a522194f18d2212ddb00f222c5ad0f62c52
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-10-21 20:32:10 +0530

    0153-find-minimum-in-rotated-sorted-array 10-21-2025

--- LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ LeetCode/Medium/0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        return nums[0];
+        for(int i = 0; i < nums.size(); i++){
+            int j = i == 0 ? j = nums.size() - 1 : i-1;
+            if(nums[j] >= nums[i])
+                return nums[i];
+        }
+        return 0;
     }
 };
