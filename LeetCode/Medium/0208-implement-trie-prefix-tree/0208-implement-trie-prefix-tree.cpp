1104b00333ca4413123b9a63e2e5986ce7df68de
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-10-29 18:25:46 +0530

    0208-implement-trie-prefix-tree 10-29-2025

--- LeetCode/Medium/0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ LeetCode/Medium/0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,39 +1,46 @@
+class TrieNode{
+    public:
+    char data;
+    TrieNode* next[26] = {};
+    bool terminal = false;
+    TrieNode(char ch){
+        data = ch;
+    }
+    TrieNode(){}
+};
+
 class Trie {
-    Trie* next[26] = {};
-    bool terminal;
+    TrieNode* node = new TrieNode();
 public:
-    
     void insert(string word) {
-        Trie* node = this;
+        TrieNode* node = this->node;
         for(char c: word){
             if(!node->next[c-'a'])
-                node->next[c-'a'] = new Trie();
-            node =  node->next[c-'a'];
+                node->next[c-'a'] = new TrieNode(c);
+            node = node -> next[c-'a'];
         }
         node->terminal = true;
     }
     
     bool search(string word) {
-        Trie* head = this;
+        TrieNode* head = node;
         for(char c: word){
-            if(!head->next[c-'a'])
+            if(!head -> next[c-'a'])
                 return false;
-            head = head->next[c-'a'];
+            head = head -> next[c-'a'];
         }
-        return head->terminal;
+        return head -> terminal;
     }
-    
     bool startsWith(string prefix) {
-        Trie* head = this;
+        TrieNode* head = node;
         for(char c: prefix){
-            if(!head->next[c-'a'])
+            if(!head -> next[c-'a'])
                 return false;
-            head = head->next[c-'a'];
+            head = head -> next[c-'a'];
         }
         return true;
     }
 };
-
 /**
  * Your Trie object will be instantiated and called as such:
  * Trie* obj = new Trie();
