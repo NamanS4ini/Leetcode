db5f4ac52a0545e08834a026e520f967035e25fe
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-10-31 21:15:24 +0530

    0079-word-search 10-31-2025

--- LeetCode/Medium/0079-word-search/0079-word-search.cpp
+++ LeetCode/Medium/0079-word-search/0079-word-search.cpp
@@ -1,29 +1,29 @@
 class Solution {
     vector<vector<int>> visited;
-    bool findWord(vector<vector<char>>& board, string& word, int br, int bc, int idx){
-        if(br >= board.size() || bc >= board[0].size() || br < 0 || bc < 0 || visited[br][bc] || board[br][bc] != word[idx])
+    bool findWord(vector<vector<char>>& board, string& word, int br, int bc,
+                  int idx) {
+        if (br >= board.size() || bc >= board[0].size() || br < 0 || bc < 0 ||
+            board[br][bc] != word[idx])
             return false;
-        visited[br][bc] = true;
-        if(idx == word.length() - 1)
+        char temp = board[br][bc];
+        board[br][bc] = '.';
+        if (idx == word.length() - 1)
             return true;
-        if(findWord(board, word, br+1, bc, idx+1))
+        if (findWord(board, word, br + 1, bc, idx + 1) ||
+            findWord(board, word, br - 1, bc, idx + 1) ||
+            findWord(board, word, br, bc + 1, idx + 1) ||
+            findWord(board, word, br, bc - 1, idx + 1))
             return true;
-        if(findWord(board, word, br-1, bc, idx+1))
-            return true;
-        if(findWord(board, word, br, bc+1, idx+1))
-            return true;
-        if(findWord(board, word, br, bc-1, idx+1))
-            return true;
-        visited[br][bc] = false;
+        board[br][bc] = temp;
         return false;
     }
+
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        visited = vector<vector<int>>(board.size(), vector<int>(board[0].size(),0));
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
-                if(board[i][j] == word[0])
-                    if(findWord(board, word, i, j, 0)){
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == word[0])
+                    if (findWord(board, word, i, j, 0)) {
                         return true;
                     }
             }
