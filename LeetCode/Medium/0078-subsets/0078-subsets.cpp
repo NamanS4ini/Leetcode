6004c12f5ed7d7528a4776ffdd173063cb7e2449
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-11-02 07:54:19 +0530

    0078-subsets 11-02-2025

--- LeetCode/Medium/0078-subsets/0078-subsets.cpp
+++ LeetCode/Medium/0078-subsets/0078-subsets.cpp
@@ -1,17 +1,13 @@
 class Solution {
-    void solve(int& i, vector<vector<int>>& ans, vector<int>& nums,
+    void solve(int i, vector<vector<int>>& ans, vector<int>& nums,
                vector<int>& curr) {
         if (i >= nums.size()) {
             ans.push_back(curr);
             return;
         }
-        i++;
-        solve(i, ans, nums, curr);
-        i--;
+        solve(i + 1, ans, nums, curr);
         curr.push_back(nums[i]);
-        i++;
-        solve(i, ans, nums, curr);
-        i--;
+        solve(i + 1, ans, nums, curr);
         curr.pop_back();
     }
 
@@ -19,8 +15,7 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> curr;
-        int index = 0;
-        solve(index, ans, nums, curr);
+        solve(0, ans, nums, curr);
         return ans;
     }
 };
