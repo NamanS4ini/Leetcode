afb8d24b23ffe99cff2e731a8557e17f95f8b1c6
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-11-02 07:31:30 +0530

    2257-count-unguarded-cells-in-the-grid 11-02-2025

--- LeetCode/Medium/2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ LeetCode/Medium/2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -3,16 +3,16 @@ public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
         vector<vector<int>> grid(m, vector<int>(n,0));
         int count = m*n;
-        for(vector<int> pos: walls){
+        for(vector<int>& pos: walls){
             grid[pos[0]][pos[1]] = -1;
             count--;
         }
-        for(vector<int> pos: guards){
+        for(vector<int>& pos: guards){
             grid[pos[0]][pos[1]] = -1;
             count--;
         }
 
-        for(vector<int> pos: guards){
+        for(vector<int>& pos: guards){
             int row = pos[0];
             int col = pos[1];
             for(int i = row - 1; i >= 0; i--){
