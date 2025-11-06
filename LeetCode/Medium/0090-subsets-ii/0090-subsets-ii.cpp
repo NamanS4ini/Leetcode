5f5c2ff616ca3197bf52158c86d9f8d4103dda2b
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-11-06 08:20:23 +0530

    0090-subsets-ii 11-06-2025

--- LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.cpp
+++ LeetCode/Medium/0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
-    void solve(vector<int>& nums, vector<vector<int>>& ans, vector<int>& output, int idx){
+    void solve(vector<int>& nums, set<vector<int>>& ans, vector<int>& output, int idx){
         if(idx >= nums.size()){
-            if(find(ans.begin(), ans.end(), output) == ans.end())
-                ans.push_back(output);
+            ans.insert(output);
             return;
         }
         solve(nums, ans, output, idx + 1);
-        output.push_back(nums[idx]);
-        solve(nums, ans, output, idx + 1);
-        output.pop_back();
+        if(idx == 0 || nums[idx] != nums[idx-1] || (output.size() && output[output.size()-1] == nums[idx])){
+            output.push_back(nums[idx]);
+            solve(nums, ans, output, idx + 1);
+            output.pop_back();
+        }
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<int> output;
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
+        set<vector<int>> ans;
         solve(nums, ans, output, 0);
-        return ans;
+        return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
