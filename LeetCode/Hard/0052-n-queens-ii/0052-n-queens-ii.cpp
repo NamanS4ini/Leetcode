163d7031a473437c59768f1dc28fd8bf1749ca38
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-11-08 15:02:16 +0530

    0052-n-queens-ii 11-08-2025

---
+++ LeetCode/Hard/0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,74 @@
+class Solution {
+      bool isSfe(vector<string>& out, int row, int col){
+        int n = out.size();
+        for(int i = 0; i < n; i++){
+            if(out[i][col] == 'Q')
+                return false;
+            if(out[row][i] == 'Q')
+                return false;
+        }
+        // UP-LEFT
+        int i = row - 1;
+        int j = col - 1;
+        while(i >= 0 && j >= 0){
+            if(out[i][j] == 'Q')
+                return false;
+            i--;
+            j--;
+        }
+        // UP-RIGHT
+        i = row - 1;
+        j = col + 1;
+        while(i >= 0 && j < n){
+            if(out[i][j] == 'Q')
+                return false;
+            i--;
+            j++;
+        }
+        // DOWN-RIGHT
+        i = row + 1;
+        j = col + 1;
+        while(i < n && j < n){
+            if(out[i][j] == 'Q')
+                return false;
+            i++;
+            j++;
+        }
+        // DOWN-LEFT
+        i = row + 1;
+        j = col - 1;
+        while(i < n && j >= 0){
+            if(out[i][j] == 'Q')
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    void solve(int& n, vector<vector<string>>& ans, vector<string>& out, int row, int col, int queens){
+        if(queens == n){
+            ans.push_back(out);
+            return;
+        }
+        for(int i = row; i < n; i++){
+            for(int j = col; j < n; j++){
+                if(isSfe(out, i, j)){
+                    queens++;
+                    out[i][j] = 'Q';
+                    solve(n, ans, out, i, j, queens);
+                    out[i][j] = '.';
+                    queens--;
+                }
+            }
+            col = 0;
+        }
+    }
+public:
+    int totalNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> out(n, string(n,'.'));
+        solve(n, ans, out, 0, 0, 0);
+        return ans.size();
+    }
+};
