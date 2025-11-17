3648b00f96b0f3d06f245241675b9dda326fcf1d
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-11-17 21:40:19 +0530

    0695-max-area-of-island 11-17-2025

--- LeetCode/Medium/0695-max-area-of-island/0695-max-area-of-island.cpp
+++ LeetCode/Medium/0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,24 +1,18 @@
 class Solution {
-    vector<vector<bool>> visited;
     int dfs(vector<vector<int>>& grid, int row, int col){
         if(row  < 0 || col < 0 || row >= grid.size() || col >= grid[0].size())
             return 0;
-        if(grid[row][col] == 0 || visited[row][col])
+        if(grid[row][col] != 1)
             return 0;
-        visited[row][col] = true;
-        int curr = dfs(grid, row - 1, col);
-        curr += dfs(grid, row + 1, col);
-        curr += dfs(grid, row, col - 1);
-        curr += dfs(grid, row, col + 1);
-        return curr + 1;
+        grid[row][col] = 2;
+        return dfs(grid, row - 1, col) + dfs(grid, row + 1, col) + dfs(grid, row, col - 1) + dfs(grid, row, col + 1) + 1;
     }
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int maxArea = 0;
-        this->visited = vector<vector<bool>>(grid.size(), vector<bool>(grid[0].size(), false));
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
-                if(!visited[i][j] && grid[i][j] == 1){
+                if(grid[i][j] == 1){
                     int curr = dfs(grid, i, j);
                     maxArea = max(curr, maxArea);
                 }
