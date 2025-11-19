4f552412692cfb35d7795b4669c5ae9b16f560aa
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-11-19 05:43:37 +0530

    2154-keep-multiplying-found-values-by-two 11-19-2025

--- LeetCode/Easy/2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
+++ LeetCode/Easy/2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        sort(nums.begin(), nums.end());
+        unordered_set<int> num;
         for(int n: nums)
-            if(n == original)
-                original *= 2;
+            num.insert(n);
+        while(num.find(original) != num.end())
+            original *= 2;
         return original;
     }
 };
