ec24a257c20664a17d8f96db99825437ffd40a82
Naman Saini <113650651+NamanS4ini@users.noreply.github.com>
2025-12-05 07:01:27 +0530

    3432-count-partitions-with-even-sum-difference 12-05-2025

--- LeetCode/Easy/3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
+++ LeetCode/Easy/3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.cpp
@@ -3,6 +3,6 @@ public:
     int countPartitions(std::vector<int>& nums) {
         int totalSum = accumulate(nums.begin(), nums.end(), 0 );
         if(totalSum%2==0) return nums.size()-1;
-        else return 0;
+        return 0;
     }
 };
